f1d039a13138abb14b5fd36858f15af7ecdc2ba1
a.bocharov <a.bocharov@msocialproduction.com>
2024-05-10 22:35:06 +0300

    add build.gradle to all projects

---
+++ spring_auth_client/src/main/java/org/example/AuthClientServerApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthClientServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthClientServerApplication.class, args);
+    }
+}
